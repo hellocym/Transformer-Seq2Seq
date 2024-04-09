5d213be4acf43141bbaa36c8dcdba867ebfc5a09
hellocym <13681921379@163.com>
2024-04-10 00:31:11 +0800

    init

--- utils.py
+++ utils.py
@@ -1,70 +1,57 @@
 import torch
-import spacy
-from torchtext.data.metrics import bleu_score
-import sys
+import torch.nn as nn
+import math
 
 
-def translate_sentence(model, sentence, german, english, device, max_length=50):
-    # Load german tokenizer
-    spacy_ger = spacy.load("de")
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, dropout=0.1, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+        self.scale = nn.Parameter(torch.ones(1))
 
-    # Create tokens using spacy and everything in lower case (which is what our vocab is)
-    if type(sentence) == str:
-        tokens = [token.text.lower() for token in spacy_ger(sentence)]
-    else:
-        tokens = [token.lower() for token in sentence]
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(
+            0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
 
-    # Add <SOS> and <EOS> in beginning and end respectively
-    tokens.insert(0, german.init_token)
-    tokens.append(german.eos_token)
-
-    # Go through each german token and convert to an index
-    text_to_indices = [german.vocab.stoi[token] for token in tokens]
-
-    # Convert to Tensor
-    sentence_tensor = torch.LongTensor(text_to_indices).unsqueeze(1).to(device)
-
-    outputs = [english.vocab.stoi["<sos>"]]
-    for i in range(max_length):
-        trg_tensor = torch.LongTensor(outputs).unsqueeze(1).to(device)
-
-        with torch.no_grad():
-            output = model(sentence_tensor, trg_tensor)
-
-        best_guess = output.argmax(2)[-1, :].item()
-        outputs.append(best_guess)
-
-        if best_guess == english.vocab.stoi["<eos>"]:
-            break
-
-    translated_sentence = [english.vocab.itos[idx] for idx in outputs]
-    # remove start token
-    return translated_sentence[1:]
+    def forward(self, x):
+        x = x + self.scale * self.pe[:x.size(0), :]
+        return self.dropout(x)
 
 
-def bleu(data, model, german, english, device):
-    targets = []
-    outputs = []
-
-    for example in data:
-        src = vars(example)["src"]
-        trg = vars(example)["trg"]
-
-        prediction = translate_sentence(model, src, german, english, device)
-        prediction = prediction[:-1]  # remove <eos> token
-
-        targets.append([trg])
-        outputs.append(prediction)
-
-    return bleu_score(outputs, targets)
+def generate_square_subsequent_mask(sz):
+    mask = torch.triu(torch.ones(sz, sz), 1)
+    mask = mask.masked_fill(mask == 1, float('-inf'))
+    return mask
 
 
-def save_checkpoint(state, filename="my_checkpoint.pth.tar"):
-    print("=> Saving checkpoint")
-    torch.save(state, filename)
+class CausalConv1d(nn.Conv1d):
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 kernel_size,
+                 stride=1,
+                 dilation=1,
+                 groups=1,
+                 bias=True):
+        self.__padding = (kernel_size - 1) * dilation
 
+        super(CausalConv1d, self).__init__(
+            in_channels,
+            out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=self.__padding,
+            dilation=dilation,
+            groups=groups,
+            bias=bias)
 
-def load_checkpoint(checkpoint, model, optimizer):
-    print("=> Loading checkpoint")
-    model.load_state_dict(checkpoint["state_dict"])
-    optimizer.load_state_dict(checkpoint["optimizer"])
+    def forward(self, input):
+        result = super(CausalConv1d, self).forward(input)
+        if self.__padding != 0:
+            return result[:, :, :-self.__padding]
+        return result
